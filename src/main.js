b97e5586e8e960c37d94a111c97ac5593c511364
Aleksey Belchenkov <u608110@gmail.com>
2020-03-06 14:41:56 +0300

    Validate Login Form

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import Vuelidate from "vuelidate";
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -12,6 +13,7 @@ import dateFilter from "./filters/date.filter";
 Vue.filter('date', dateFilter);
 
 Vue.config.productionTip = false;
+Vue.use(Vuelidate);
 
 new Vue({
   router,
